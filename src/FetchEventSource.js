7072a8ddc8c090c869ac464b8ac9398053c52c94
Daniel Baulig <me@danielbaulig.de>
2023-11-03 10:36:20 -0700

    Add a polyfill FetchEventSource for testing Private Network Access fetch APIs

---
+++ src/FetchEventSource.js
@@ -0,0 +1,160 @@
+function isComment(value) {
+  return value.startsWith(':');
+}
+
+function isTermination(value) {
+  // We split by \n, so we expect the empty line to be an empty string
+  return value === '';
+}
+
+const fieldRegex = /^([a-z]+):\s?(.*)$/;
+
+function isField(value) {
+  return !!value.match(fieldRegex);
+}
+
+function getFieldNameAndValue(value) {
+  const match = value.match(fieldRegex);
+  return [match[1], match[2]];
+}
+
+function createMessage() {
+  return {
+    data: [],
+    event: 'message',
+  };
+}
+
+const READYSTATE_CONNECTING = 0;
+const READYSTATE_OPEN = 1;
+const READYSTATE_CLOSED = 2;
+
+export default class EventSource extends EventTarget {
+  constructor(url, {fetchOptions} = {}) {
+    super();
+    this._allowCors = false;
+    this._abortController = new AbortController();
+    const { signal } = this._abortController;
+    this._fetchOptions = Object.assign({
+      signal,
+    }, fetchOptions);
+    this._url = url;
+    this._lastEventId = '';
+    this._retryTimeout = 30000;
+    this._connect();
+  }
+
+  _getFetchHeaders() {
+    if (this._allowCors) {
+      return {
+        'Last-Event-ID': this._lastEventId,
+        'Accept': 'text/event-stream',
+      };
+    } 
+    return {};
+  }
+
+  _connect() {
+    this.readyState = READYSTATE_CONNECTING;
+    fetch(this._url, { ...this._fetchOptions, headers: this._getFetchHeaders()}).then(this._onResponse, this._onError);
+  }
+
+  close() {
+    this._abortController.abort();
+  }
+
+  _onError = (e) => {
+    if (e.name === 'AbortError') {
+      this.readyState = READYSTATE_CLOSED;
+      return;
+    }
+    console.error(e);
+    this.dispatchEvent(new Event('error'));
+    setTimeout(this._retryTimeout, () => {
+      this._connect();
+    });
+    this.readyState = READYSTATE_CONNECTING;
+  }
+
+  _onMessageComplete(message) {
+    const dataField = message.data;
+    const data = dataField.length > 0 ? dataField.join('\n') : null;
+    const lastEventId = this._lastEventId;
+    const messageEvent = new MessageEvent(message.event, {data, lastEventId});
+    console.log(messageEvent);
+    this.dispatchEvent(messageEvent);
+  }
+
+  _onConnected = () => {
+    this.readyState = READYSTATE_OPEN;
+    this.dispatchEvent(new Event('open'));
+  }
+
+  _onResponse = async (response) => {
+    if (!response.ok) {
+      return this._onError(response);
+    }
+
+    this._onConnected();
+    
+    try {
+      const stream = await response.body;
+      const textStream = stream.pipeThrough(new TextDecoderStream());
+      const reader = textStream.getReader();
+
+      let message = createMessage();
+      do {
+        const {done, value} = await reader.read();
+        if (done) {
+          break;
+        }
+
+
+        const normalized = value.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
+
+        console.log('value', normalized);
+        if (!normalized.endsWith('\n')) {
+          console.warn('Message wasn\'t terminated by a newline character');
+          continue;
+        }
+        const lines = normalized.substr(0, normalized.length  - 1).split('\n');
+        console.log('lines', lines);
+
+        lines.forEach((value) => {
+          if (isComment(value)) {
+            return;
+          }
+
+          if (isField(value)) {
+            const [fieldName, fieldValue] = getFieldNameAndValue(value);
+
+            switch (fieldName) {
+              case 'data':
+                message.data.push(fieldValue);
+                break;
+              case 'id':
+                this._lastEventId = fieldValue;
+                break;
+              case 'retry':
+                this._retryTimeout = parseInt(fieldValue);
+                break;
+              case 'event':
+                message.event  = fieldValue;
+                break;
+            }
+            return;
+          }
+
+          if (isTermination(value)) {
+            this._onMessageComplete(message);
+            message = createMessage();
+            return;
+          }
+        });
+      } while(true);
+    } catch(e) {
+      this._onError(e);
+    }
+  }
+}
+globalThis.EventSource = EventSource;
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import EventSource from './FetchEventSource';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ControllerList from './ui/ControllerList.jsx';
