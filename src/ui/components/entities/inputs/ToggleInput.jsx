0607f4653dca385662a05209dc3007bb311508f5
Daniel Baulig <me@danielbaulig.de>
2024-02-06 14:26:12 -0800

    Make SwitchEntity toggleable

--- src/ui/components/entities/inputs/ToggleInput.jsx
+++ src/ui/components/entities/inputs/ToggleInput.jsx
@@ -1,8 +1,12 @@
 import { toggle } from './ToggleInput.module.css';
 
 export default function ToggleInput({value, onChange}) {
-  return <label className={toggle}><input type="checkbox" className={toggle}
-    value={value}
-    onChange={onChange}
-  /></label>;
+  return <label className={toggle}>
+    <input
+      type="checkbox"
+      className={toggle}
+      checked={value}
+      onChange={onChange}
+    />
+  </label>;
 }
